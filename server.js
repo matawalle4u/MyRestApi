f4ac848de505976297d9ec3536f9587d97ec0d67
Adam <matawalle4u@gmail.com>
2021-05-31 21:53:49 +0100

    Use regex to validate API url pattern instead of fixed url

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+
 const PORT = 5000
 const http = require('http')
-const {getItems} = require('./controllers/itemsControllers')
+const {getItems, getItem} = require('./controllers/itemsControllers')
 
 const server = http.createServer((req, res)=>{
 
     if(req.url==='/api/items'){
+        //Use Regex to match instead of fixed string
         getItems(req, res)
         
+
+    }else if(req.url.match('/\api/\([aA-zZ]+)\/([0-9])') && req.method==='GET'){
+        const id = req.url.split('/')[3]
+        getItem(req, res, id)
+
+    }else if(req.url==='/api/items' && req.method==='POST'){
 
     }else{
         res.writeHead(404, {'Content-type':'application/json'})
